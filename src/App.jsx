81e243247aafcef9033db139ebe1b0883f30d9e8
devpcjin <sgn03077@gmail.com>
2023-12-20 22:34:29 +0900

    feat: prettier / tailwind 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div>
-      <p className="font-bold">안녕하세요</p>
-    </div>
-  );
+    return (
+        <div>
+            <p className="p-10 text-2xl text-amber-200">안녕하세요</p>
+        </div>
+    );
 }
 
 export default App;
